414f2a850337a997dea2a451f6eb1d4cc8ffbec1
Steins <stienrei@gmail.com>
2026-07-20 15:00:58 +0800

    Initial commit from Create Next App

---
+++ README.md
@@ -0,0 +1,8 @@
+# Default Starter Template
+by [ STEINS REI ]
+
+## This template includes:
+- Folder structures
+- Pre-installed packages
+- Configured config files
+- Pre-built mixins
---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+import path from 'path';
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.join(__dirname),
+  },
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+};
+
+export default nextConfig;
+
