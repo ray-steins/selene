f1899b6a2f19e63dfc2cf26e593519de5f683076
Steins <stienrei@gmail.com>
2026-08-06 14:11:27 +0800

    fixes to deployment error

--- src/app/(main)/Home.tsx
+++ src/app/(main)/Home.tsx
@@ -6,57 +6,10 @@ import { AssignmentWithClass } from "@/lib/assignments"
 import { UserClasses } from "@/data/user"
 import { ROUTES } from "@/configs/app.config"
 
-function AssignmentDisplaySection({ classes }: { classes: UserClasses }) {
-  if (!classes || classes.length === 0) return <h1>You are not in any class.</h1>
-
-  const assignments: AssignmentWithClass[] = classes.flatMap(v => v.assignments);
-
+export default function HomePageClient() {
   return (
     <div>
-      <h2>Assignments</h2>
-      <AssignmentDisplay assignments={assignments}/>
-    </div>
-  )
-}
-
-function ClassDisplaySection({ classes }: { classes: UserClasses }) {
-  const Display = () => {
-    return (
-      <>
-        {classes?.map((v, i) => {
-          const link = `${ROUTES.dashboard.classes}${v.slug}`
-          
-          return (
-            <a href={link} key={`${v}-${i}`}>
-              <div>
-                <span>{ v.name }</span>
-              </div>
-            </a>
-          )
-        })}      
-      </>
-    )
-  }
-
-  return (
-    <div>
-      <h1>Classes</h1>
-      <Display />
-    </div>
-  )
-}
-
-export default function HomePageClient({
-  user,
-  classes
-}: {
-  user: User
-  classes: UserClasses
-}) {
-  return (
-    <div>
-      <AssignmentDisplaySection classes={classes}/>
-      <ClassDisplaySection classes={classes}/>
+      <h1>Welcome to Selene!</h1>
     </div>
   )
 }
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,19 +1,9 @@
-import { AssignmentDisplay } from "./dashboard/components/ui/AssignmentDisplay";
-import { getCurrentUserFromServer, getUserClasses } from "@/data/user";
 import HomePageClient from "./Home";
 
 export default async function MainPage() {
-  const currentUser = await getCurrentUserFromServer();
-  if (!currentUser) return <span>Not signed in yet.</span>
-
-  const userClasses = await getUserClasses(currentUser.id) ?? [];
-
   return (
     <>
-      <HomePageClient 
-        user={currentUser}
-        classes={userClasses}
-      />
+      <HomePageClient />
     </>
   )
 }
